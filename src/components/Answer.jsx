4f48d83a3b3c82108d358ccb1f7075990cf6a407
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-09 22:45:35 +0100

    Move handleAnswerClick to Answer.jsx

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react'
+
 export default function Answer(props) {
-    return (
-        <button className="question__option" onClick={props.toggleSelect}>{props.value}</button>
-    )
+  const [answerClasses, setAnswerClasses] = useState(['question__option'])
+
+  const handleAnswerClick = event => {
+    setAnswerClasses(prevAnswerClasses => {
+      console.log(prevAnswerClasses)
+      // First letting user select / deselect what they want
+      if (prevAnswerClasses.includes('question__option--selected')) {
+        // Add selected modifier class
+        prevAnswerClasses.push('question__option--selected')
+      } else {
+        // Remove selected modifier class
+        const index = prevAnswerClasses.indexOf('question__option--selected')
+        prevAnswerClasses.splice(index, 1)
+      }
+
+      // Check which answer has the selected class
+
+    })
+  }
+
+  console.log(answerClasses);
+
+  return (
+    <button className={answerClasses.join(" ")} onClick={handleAnswerClick}>
+      {props.value}
+    </button>
+  );
 }
