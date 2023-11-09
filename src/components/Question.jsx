c034a437dfebdc5436e6c4949e3e6f900d6d4e8e
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-09 21:55:24 +0100

    Let user add/remove selected modifier class

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,6 +2,33 @@ import { nanoid } from 'nanoid'
 import Answer from './Answer'
 
 export default function Question(props) {
+  /* =========================
+    New code - start
+  ========================= */
+  
+  const [answerClasses, setAnswerClasses] = useState(['question__option'])
+
+  const handleClick = event => {
+    setAnswerClasses(prevAnswerClasses => {
+      // First letting user select / deselect what they want
+      if (prevAnswerClasses.contains('question__option--selected')) {
+        // Add selected modifier class
+        prevAnswerClasses.push('question__option--selected')
+      } else {
+        // Remove selected modifier class
+        const index = prevAnswerClasses.indexOf('question__option--selected')
+        prevAnswerClasses.splice(index, 1)
+      }
+
+      // Check which answer has the selected class
+
+    })
+  }
+
+  /* =========================
+    New code - end
+  ========================= */
+
   // Create four answers
   const allAnswers = props.answers.map((answer) => {
     return <Answer key={nanoid()} toggleSelect={toggleSelect} value={answer} />
@@ -22,7 +49,8 @@ export default function Question(props) {
     })
 
     if (selectedIndex) {
-      // If some answer was clicked && something has the selected class (user didn't just deselect) -> remove all selections, then select the target (to make sure only 1 answer is selected)
+      // If some answer was clicked && something has the selected class (user didn't just deselect) 
+      // -> remove all selections, then select the target (to make sure only 1 answer is selected)
       answers.forEach((answer) => answer.classList.remove('question__option--selected'))
       event.target.classList.add('question__option--selected')
     }
