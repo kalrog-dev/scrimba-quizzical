b506b49676ae0236106d95823c39aa5683638f6a
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-10 22:29:51 +0100

    Fix indentation and remove an unnecessary useState import

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,24 +1,23 @@
-import { useState } from "react";
 import { nanoid } from 'nanoid'
 import Answer from './Answer'
 
 export default function Question(props) {
-  // Store which of the 4 corresponding answers has been selected (null / value)
+  // Store which one of the 4 corresponding answers has been selected (null / value)
   const selectedAnswer = props.selectedAnswers[props.questionIndex]
 
   // Create four answers
-  const allAnswers = props.answers.map((answer) => {
-    return <Answer 
-            key={nanoid()} 
-            value={answer} 
-            showAnswers={props.showAnswers}
-            correctAnswer={props.correct}
-            selectedAnswer={selectedAnswer}
-            selectedAnswers={props.selectedAnswers}
-            setSelectedAnswers={props.setSelectedAnswers}
-            questionIndex={props.questionIndex}
-          />
-  })
+  const allAnswers = props.answers.map((answer) => 
+    <Answer 
+      key={nanoid()} 
+      value={answer} 
+      showAnswers={props.showAnswers}
+      correctAnswer={props.correct}
+      selectedAnswer={selectedAnswer}
+      selectedAnswers={props.selectedAnswers}
+      setSelectedAnswers={props.setSelectedAnswers}
+      questionIndex={props.questionIndex}
+    />
+  )
 
   return (
       <section className="question">
